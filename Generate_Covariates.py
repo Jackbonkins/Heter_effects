594402e877eae5d5df972f10848717615a5a96a0
Joao Andrade <joaovitorpais@hotmail.com>
2024-04-14 21:18:59 +0200

    Covariate interactions added

--- Generate_Covariates.py
+++ Generate_Covariates.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 
+
 #Generate covariance matrix
 def gen_cov_matrix(p):
-
+    
     P = p # number of features/variables in the data set (complexity)
     a = np.random.rand(P, P)
     m = np.tril(a) + np.tril(a, -1).T
@@ -27,7 +28,7 @@ def positive_definite(matrix):
 
 # Generate covariates from a normal distribution using the covariance matrix
 def gen_covariates(p, cov_matrix, N):
-       
+
     mean=np.zeros(p, dtype=int)
     X = np.random.default_rng().multivariate_normal(mean, cov_matrix, N)
     covariates = pd.DataFrame(X)
